15afd51fc1bab86247ab5fae5b47c7e821240839
Roninja <malsa080@uottawa.ca>
2021-07-20 19:32:56 -0400

    classfinder layout
    finished 3 pages of class finder layout

---
+++ app/src/main/java/com/example/mustafabranch/WebLinks.java
@@ -0,0 +1,13 @@
+package com.example.mustafabranch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebLinks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_links);
+    }
+}
