b8f907552fb8572e229da507c3f8868c2c8367c9
Himanshu Manhas <55300303+ManhasHimanshu@users.noreply.github.com>
2021-08-08 16:38:59 -0400

    added functionality to buttons on main page

--- app/src/main/java/com/example/mustafabranch/MainScreen.java
+++ app/src/main/java/com/example/mustafabranch/MainScreen.java
@@ -1,13 +1,45 @@
 package com.example.mustafabranch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
+
+    ImageButton destination;
+    ImageButton search;
+    ImageButton favourite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
+        destination = (ImageButton) findViewById(R.id.imageButton4);
+        destination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(MainScreen.this, PickDestination.class);
+                startActivity(changePage);
+            }
+        });
+        search = (ImageButton) findViewById(R.id.imageButton3);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(MainScreen.this, SearchActivity.class);
+                startActivity(changePage);
+            }
+        });
+        favourite = (ImageButton) findViewById(R.id.imageButton5);
+        favourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(MainScreen.this, FavouritesPage.class);
+                startActivity(changePage);
+            }
+        });
     }
 }
