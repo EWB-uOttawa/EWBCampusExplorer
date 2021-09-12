dfff33d05b0bdedb58236bd23bc132abe3c1dd4f
Himanshu Manhas <55300303+ManhasHimanshu@users.noreply.github.com>
2021-09-12 14:12:47 -0400

    finished buttons on main page

--- app/src/main/java/com/example/myapplication/mainPage.java
+++ app/src/main/java/com/example/myapplication/mainPage.java
@@ -10,17 +10,35 @@ import android.widget.ImageButton;
 public class mainPage extends AppCompatActivity {
 
     ImageButton destination;
+    ImageButton building;
+    ImageButton favourites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
 
+        building = (ImageButton) findViewById(R.id.imageButton3);
         destination = (ImageButton) findViewById(R.id.imageButton4);
+        favourites = (ImageButton) findViewById(R.id.imageButton5);
+        building.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(mainPage.this, chooseBuilding.class);
+                startActivity(changePage);
+            }
+        });
         destination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent changePage = new Intent(mainPage.this, PickDestination.class);
+                startActivity(changePage);
+            }
+        });
+        favourites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePage = new Intent(mainPage.this, favouritesPage.class);
                 startActivity(changePage);
             }
         });
